d314100a11c2513adb0950d88c2630a9e981e366
Brian Rubio <brian.rubio@accedo.tv>
2021-03-21 21:01:38 -0600

    Add UPDATE and DELETE

--- src/models/Cake.js
+++ src/models/Cake.js
@@ -1,31 +1,40 @@
 import {v4 as uuidv4} from 'uuid';
 
-const CAKES = {
-  '1': {
-    id: '1',
-    name: 'Chocolate',
-    price: 300,
-    flavors: ['milk', 'chocolate'],
-  },
-  '2': {
-    id: '2',
-    name: 'Carrot',
-    price: 300,
-    flavors: ['milk', 'carrot'],
-  },
+let CAKES = {
+    '1': {
+        id: '1',
+        name: 'Chocolate',
+        price: 150,
+        flavors: ['milk', 'chocolate'],
+    },
+    '2': {
+        id: '2',
+        name: 'Carrot',
+        price: 300,
+        flavors: ['milk', 'carrot'],
+    },
 };
 
 export default {
-  all: () => Object.values(CAKES),
-  get: (id) => CAKES[id],
-  create: (data) => {
-    const newCake = {
-      ...data,
-      id: uuidv4(),
-    };
+    all: () => Object.values(CAKES),
+    get: (id) => CAKES[id],
+    create: (data) => {
+        const newCake = {
+            ...data,
+            id: uuidv4(),
+        };
 
-    CAKES[newCake.id] = newCake;
+        CAKES[newCake.id] = newCake;
 
-    return newCake;
-  },
+        return newCake;
+    },
+    update: (id, data) => {
+      CAKES[id] = {id, ...data};
+      return CAKES[id];
+    },
+    remove: (id) => {
+      const {[id]: value, ...otherCakes} = CAKES;
+      CAKES = otherCakes
+      return otherCakes
+    }
 };
--- src/routes/cakes.js
+++ src/routes/cakes.js
@@ -39,4 +39,44 @@ router.post('/', (req, res, ) => {
   }
 });
 
+router.put('/:id', (req, res, next) => {
+  const cake = Cake.get(req.params.id);
+
+  if (cake == null){
+    res.status(404).json({message: 'Cake not found'});
+  }
+
+  const body = req.body;
+
+  let updatedCake = {
+    name: body.name ? body.name: cake.name,
+    price: body.price ? body.price : cake.price,
+    flavors: body.flavors ? body.flavors : cake.flavors
+  }
+
+  try {
+    updatedCake = Cake.update(req.params.id, updatedCake);
+
+    res.json(updatedCake);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+router.delete('/:id', (req, res, next) => {
+  const cake = Cake.get(req.params.id);
+
+  if (cake == null){
+    res.status(404).json({message: 'Cake not found'});
+  }
+
+  try {
+    Cake.remove(req.params.id)
+
+    res.json({message: 'Caked removed'});
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
 export default router;
